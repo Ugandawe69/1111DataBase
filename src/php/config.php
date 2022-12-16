0058eb5a4862c645763be91189f3a0586f059134
lin121291 <4jp33f9e@gmail.com>
2022-12-17 01:16:19 +0800

    version 1.1

--- src/php/config.php
+++ src/php/config.php
@@ -1,3 +1,14 @@
 <?php
-//連接資料庫
+/* Attempt to connect to MySQL database */
+$link = new mysqli('mysql_db','root','root','homework');
+// 輸入中文也OK的編碼
+mysqli_query($link, 'SET NAMES utf8');
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+else{
+    return $link;
+}
 ?>
--- src/php/main.php
+++ src/php/main.php
@@ -1,13 +1,10 @@
 <?php
-    $json=array();
 
-    if($_SERVER['REQUEST_METHOD']=="POST"){
-        // 換成php://input 
-        $inputJson = file_get_contents('php://input');
-        if(!empty(json_decode($inputJson))){
-            //......
-        }else{
-            $json = array("status" => 1, "msg" => "inputJson is empty");
-        }
-    }
+    //connect to database
+    $conn=require_once("config.php");
+    //接收POST
+    $username=$_POST['username'];
+
+
+    echo $username
 ?>
